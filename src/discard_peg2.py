b7929192b375cfbce8e13ea54a0f17997a9a2ed6
Beatrice Mossberg <beatrice.mossberg@gmail.com>
2025-04-24 11:57:44 +0200

    discard turn correction

--- src/discard_peg2.py
+++ src/discard_peg2.py
@@ -60,19 +60,19 @@ class DiscardPegRoutine:
         rospy.loginfo("Starting discard peg routine")
 
         # 1. Turn right 90 degrees
-        self.turn(angular_speed=-0.5, duration=10)
+        self.turn(angular_speed=-0.5, duration=3.2)
 
         # 2. Move forward 30 cm
-        self.move(speed=0.1, duration=5)
+        self.move(speed=0.1, duration=3.5)
 
         # 3. Open gripper
         self.open_gripper()
 
         # 4. Move backward 30 cm
-        self.move(speed=-0.1, duration=5)
+        self.move(speed=-0.1, duration=3.5)
 
         # 5. Turn left 90 degrees
-        self.turn(angular_speed=0.5, duration=10)
+        self.turn(angular_speed=0.5, duration=3.2)
 
         rospy.loginfo("Routine complete")
 
